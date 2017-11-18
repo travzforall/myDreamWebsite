21f73b6e1fe05ec70eab99f50fe1a449d4ae3ab5
ACE <acetip@aces-mbp.lab.lagcc>
2017-11-17 21:05:12 -0500

    Made aweseom changes

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var mySwitch = document.getElementsByClassName(".row");
+var tab = document.querySelectorAll(".about");
+
+
+console.log(mySwitch);
+// mySwitch.addEventListener('click', function(event) {
+//
+// });
